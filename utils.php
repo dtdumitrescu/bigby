a5d3ed4c652fa1413353fd48c4cb41e349510414
DT Dumitrescu <dtdumitrescu@gmail.com>
2014-08-10 16:31:09 -0400

    Syntax fix to BgbUtils class

--- utils.php
+++ utils.php
@@ -298,13 +298,13 @@ class BgbUtils {
     return (substr($haystack, -$length) === $needle);
   }
 
-  function from_camel_case($str) {
+  public static function from_camel_case($str) {
     $str[0] = strtolower($str[0]);
     $func = create_function('$c', 'return "_" . strtolower($c[1]);');
     return preg_replace_callback('/([A-Z])/', $func, $str);
   }
  
-  function to_camel_case($str, $capitalise_first_char=false) {
+  public static function to_camel_case($str, $capitalise_first_char=false) {
     if($capitalise_first_char) {
       $str[0] = strtoupper($str[0]);
     }
